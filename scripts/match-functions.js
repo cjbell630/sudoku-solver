a9d9d34aa9121bc5d3b1269e4735ca676d7e7aed
Connor Bell <cjbell630@users.noreply.github.com>
2020-10-07 17:06:32 -0400

    updated for easier documentation generation

--- scripts/match-functions.js
+++ scripts/match-functions.js
@@ -1,10 +1,6 @@
-/* Put all functions you want the script to check for matches against in this array! */
-let matchFuncsToUse = [row, column, square]
-
-
-/* MATCHING FUNCTIONS */
-/*---------------*/
 /**
+ * MATCHING FUNCTIONS
+ * -----------------
  * A valid matching function must take a 1d length 4 array of numbers (the tile position) and return another array
  * which dictates which positions may not hold that value. Don't worry about not including the position itself, that is
  * handled in solver.js.
@@ -15,36 +11,44 @@ let matchFuncsToUse = [row, column, square]
  * Vertically adjacent tiles are similar to matching columns... they all share the same x and a, and their position is +/-1 off of the center.
  * This can be represented like so:
  * TODO
+ *
+ *
+ * NOTE: They don't technically have to be in this class, it just makes it easier to generate documentation this way.
  */
-/*---------------*/
+class MatchFunctions {
+    /* vanilla sudoku */
+    static row = function (position) {
+        return [position[0], -1, position[2], -1];
+    }
+    static column = function (position) {
+        return [-1, position[1], -1, position[3]];
+    }
+    static square = function (position) {
+        return [position[0], position[1], -1, -1];
+    }
 
-/* vanilla sudoku */
-row = function (position) {
-    return [position[0], -1, position[2], -1];
-}
-column = function (position) {
-    return [-1, position[1], -1, position[3]];
-}
-square = function (position) {
-    return [position[0], position[1], -1, -1];
-}
+    /* other examples */
+    static kingsMove = function (position) {
+        // TODO: do
+        let directions = [
+            function (position) {
+                return []
+            },
+            function (position) {
+            },
+            function (position) {
+            },
+            function (position) {
 
-/* other examples */
-kingsMove = function (position) {
-    // TODO: do
-    let directions = [
-        function (position) {
-        return []
-        },
-        function (position) {
-        },
-        function (position) {
-        },
-        function (position) {
+            }
+        ]
+        return [position[0], position[1], -1, -1];
+    }
 
-        }
-    ]
-    return [position[0], position[1], -1, -1];
+
+
+    /**
+     * Put all functions you want the script to check for matches against in this array!
+     */
+    static matchFuncsToUse = [MatchFunctions.row, MatchFunctions.column, MatchFunctions.square]
 }
-
-/*---------------*/
