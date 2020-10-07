d282122116441a56ba84a46bdadc7e9c3c691bab
Connor Bell <cjbell630@users.noreply.github.com>
2020-10-06 22:34:26 -0400

    added comments/documentation and renamed some variables

--- solver.js
+++ solver.js
@@ -1,4 +1,10 @@
-function solve(table) {
+/**
+ * solves the given sudoku board and returns the computed solution.
+ * @param board {[[[[string]]]]} - the 4d array of strings to solve ([y][x][b][a])
+ * @returns {[[[[string]]]]} - the solved 4d array
+ */
+function solve(board) {
+    //TODO: change param and return from string[][][][] to int[][][][]
     //TODO: actually solve
-    return table; //solved
+    return board; //solved
 }
--- tools.js
+++ tools.js
@@ -1,66 +1,73 @@
+/**
+ * builds (displays) either an empty table or one based on the given board.
+ * @param board {[[[[string]]]]} - the 4d array of strings to build the table from ([y][x][b][a])
+ */
 function buildTable(board = undefined) {
     let basetile = document.getElementById("basetile");
     basetile = basetile.cloneNode(true);
-    basetile.removeAttribute("hidden");
+    basetile.removeAttribute("hidden"); //unhide any tiles cloned from this one (but keep the original hidden)
     if (document.getElementById("maintable") != null) {
-        document.getElementById("maintable").remove();
+        document.getElementById("maintable").remove(); //remove any preexisting table
     }
-    let table = document.createElement("table");
+    let table = document.createElement("table"); // create the new one
     table.className = "maintable"
     table.id = "maintable";
-    //TODO: verify var naming
     let tile;
-    let row;
-    let cell;
+    let row_of_squares;
+    let square_holder;
     let square;
-    let sqrow;
-    let sqcell;
-    for (let y = 0; y < 3; y++) { //rows of squares
-        row = table.insertRow();
-        for (let x = 0; x < 3; x++) { //columns of squares
-            cell = row.insertCell();
+    let row_of_tiles;
+    let tile_holder;
+    for (let y = 0; y < 3; y++) { // rows of squares
+        row_of_squares = table.insertRow();
+        for (let x = 0; x < 3; x++) { // squares
+            square_holder = row_of_squares.insertCell();
             square = document.createElement("table");
-            square.className = "innertable"
+            square.className = "innertable" //TODO: necessary?
             for (let b = 0; b < 3; b++) { // rows of tiles within square
-                sqrow = square.insertRow();
-                for (let a = 0; a < 3; a++) { // columns of tiles within square
-                    sqcell = sqrow.insertCell();
-                    tile = basetile.cloneNode(true);
-                    if (board !== undefined) {
+                row_of_tiles = square.insertRow();
+                for (let a = 0; a < 3; a++) { // tiles within square
+                    tile_holder = row_of_tiles.insertCell();
+                    tile = basetile.cloneNode(true); //copies the unhidden base tile
+                    if (board !== undefined) { // if the user did not supply a board parameter
                         tile.innerText = board[y][x][b][a];
+                        tile.setAttribute("contenteditable", false); //TODO: decide later
                     }
-                    sqcell.appendChild(tile);
+                    tile_holder.appendChild(tile); // puts the tile in the tile holder (cell)
                 }
             }
-            cell.appendChild(square);
+            square_holder.appendChild(square); // puts the square in the square holder (cell)
         }
     }
-    document.body.appendChild(table);
+    document.body.appendChild(table); //adds the table to the document
 }
 
+/**
+ * adds the key restrictions (only 1-9, only length 0-1) to every element with the class name "sudokutile".
+ */
 function addKeyRulesToTiles() {
     Array.from(document.getElementsByClassName("sudokutile")).forEach(function (element) {
-        console.log("adding event listener")
         element.addEventListener("keypress", function (event) {
-            console.log("key pressed")
-            let key = event.key === undefined ? event.code : event.key;
-            if (/^[1-9]/.test(key)) { //if the key can be pressed
-                element.innerText = "";
-                return true;
+            let key = event.key === undefined ? event.code : event.key; // should get the correct value for most browsers
+            if (/^[1-9]/.test(key)) { // if the key can be pressed
+                element.innerText = ""; // remove all text
+                return true; // probably unnecessary
             } else {
-                event.preventDefault();
-                return false;
+                event.preventDefault(); // don't allow the key to be pressed
+                return false; // probably unnecessary
             }
         })
     });
 }
 
+/**
+ * collects the information in the displayed table into a more readable array.
+ */
 function collect_and_solve() {
     let board = [];
     let row_of_squares;
     let square;
     let row_of_tiles;
-    //collect data
     let table = document.getElementById("maintable")
     Array.from(table.rows).forEach(function (y) {
         board.push([]);
@@ -72,11 +79,11 @@ function collect_and_solve() {
                 square.push([]);
                 Array.from(a.cells).forEach(function (b) {
                     row_of_tiles = square[square.length - 1]
-                    row_of_tiles.push(b.innerText);
+                    row_of_tiles.push(b.innerText); //TODO: cast to int
                 })
             })
         })
     })
-    console.log(board);
+    console.log(board); //TODO: remove when finished
     buildTable(solve(board));
 }
